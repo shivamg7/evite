2506e96579f05d0928e416a80048437fc3aae84a
shivamg7 <playfullittlekid@gmail.com>
2019-03-30 06:49:17 +0530

    made migrations

--- evite/models.py
+++ evite/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
 # Create your models here.
+
+class organiser(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    company = models.CharField(max_length=20)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class participant(models.Model):
+
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    phone = models.CharField(primary_key=True, max_length=10)
+
+    def __str__(self):
+        return self.name
+
 
 class event(models.Model):
 
@@ -17,33 +39,13 @@ class event(models.Model):
     name = models.CharField(max_length=30)
     startDate = models.DateField(default=datetime.today)
     endDate = models.DateField(default=datetime.today)
-    city = models.CharField(default='Bangalore')
+    city = models.CharField(max_length=20,default='Bangalore')
     Venue = models.TextField()
     category = models.CharField(max_length=4,choices=CATEGORY_CHOICES)
-    createdBy = models.ForeignKey(organiser,default=None)
+    createdBy = models.ForeignKey(organiser,default=None,on_delete=models.CASCADE)
     ticketPrice = models.IntegerField(default=0)
     image = models.ImageField(blank=True)
     description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class organiser(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=40)
-    company = models.CharField(max_length=20)
-    image = models.ImageField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-class participant(models.Model):
-    id = models.AutoField()
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=40)
-    phone = models.CharField(primary_key=True, max_length=10)
 
     def __str__(self):
         return self.name
